a5cdf96e71eb546f78856ea8336fbee37da8a3e8
Vladislav <92696001+PocketBrainCorp@users.noreply.github.com>
2022-03-21 16:50:47 +0300

    Add files via upload

---
+++ project/include/utils.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT_INCLUDE_UTILS_H_
+#define PROJECT_INCLUDE_UTILS_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+// добавляем все нужные библиотеки
+
+// проинициализировать функции
+
+size_t timer_from(int from);
+int custom_power(int base, int power);
+
+#endif  // PROJECT_INCLUDE_UTILS_H_
